fb446cdf1101081a387c0c05c75da15904e242f1
fam246 <ola.fam10@gmail.com>
2019-04-12 22:35:35 +0100

    More

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import {Query} from 'react-apollo';
+import ErrorMessage from './error';
 
 
 const GET_COUNTRIES_CODE = gql`
@@ -28,6 +29,68 @@ onCountryChange = event => {
   render() {
     return (
       <div >
+        <Query query={GET_COUNTRIES_CODE}>
+
+         {({data, loading, error})=>{
+
+          if(loading) return <p>Loading...</p>;
+          if (error) return <ErrorMessage error={error}/>;
+          return(
+            <div>
+            <select value={this.state.country} onChange={this.onCountryChange}>
+              {data.countries.map(country => (
+               <option key={country.code} value={country.code}>
+
+                  {country.name}
+
+
+               </option>
+
+
+
+
+
+
+              ))}
+            </select>
+
+            <br/>
+
+            <li>
+             
+             {this.state.country}
+  
+            </li>
+
+
+
+
+
+            </div>
+
+
+
+
+
+
+
+          )
+
+
+
+
+
+
+
+
+
+
+         }}
+
+
+
+
+        </Query>
         
       </div>
     );
